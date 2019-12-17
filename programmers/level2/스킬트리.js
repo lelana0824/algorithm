d28453fdbbbc82128c2b090d989f062617223fdb
lelana0824 <lelana0824@gmail.com>
2019-12-17 13:11:42 +0900

    스킬트리 설명 추가

--- programmers/level2/스킬트리.js
+++ programmers/level2/스킬트리.js
@@ -1,6 +1,8 @@
 function solution(skill, skill_trees) {
   let count = 0;
 
+  // 1. 일단 순서에 상관없이 배울 수 있는 스킬들은 제거를 한다.
+  // skill_tress의 요소인 문자열 중 skill 문자열에 해당되지 않는 문자열을 삭제하는 과정이다.
   const onlySkillTreeList = skill_trees.map(char => {
     return char
       .split("")
@@ -8,7 +10,10 @@ function solution(skill, skill_trees) {
       .join("");
   });
 
+  console.log(onlySkillTreeList);
+
   onlySkillTreeList.forEach(char => {
+    console.log(`${char} === ${skill.substring(0, char.length)}`);
     if (char === skill.substring(0, char.length)) {
       count++;
     }
