3aa2b9c3ce307c72a7b154240d67e84624e82113
lelana0824 <lelana0824@gmail.com>
2019-12-19 10:23:00 +0900

    기능개발 주석 추가

--- programmers/level2/기능개발.js
+++ programmers/level2/기능개발.js
@@ -1,5 +1,7 @@
 function solution(progresses, speeds) {
   let answer = [];
+
+  // speed와 더해서 객체를 만들어 줌
   let deployList = progresses.map((v, i) => {
     return {
       progress: v,
@@ -21,11 +23,17 @@ function solution(progresses, speeds) {
         if (deployList[i].progress >= 100) {
           count++;
         } else {
+          // 이때 주의해야 할 점은 모든 요소를 돌면 안됨. 왜냐면 배열의 순서대로 배포를 하는게 원칙이기 때문에
+          // 100, 130, 56, 100, 40 이라면 모두 for문을 돌아서 count가 3이 되는게 아니라
+          // 100, 130 까지만 돌아서 count가 2가 되어야 함. 왜냐면 4번째에 있는 기능은 3번째에 있는 기능 개발이 56까지 밖에 안됐으므로 배포를 못함
           break;
         }
       }
 
+      // 배포가 완료된 요소는 제거
       deployList = deployList.splice(count);
+
+      // 배포가 된 기능의 갯수를 더해줌.
       answer.push(count);
     }
   }
